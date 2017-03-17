a3f41e121a69ecaed9cb10ee18b8849c6e5a79cf
serge1peshcoff <serge1peshcoff@mail.ru>
2017-03-17 23:19:13 +0300

    ESLint improvement and some small refactoring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const redis = require('redis');
-const nconf = require('nconf');
-const blessed = require('blessed')
-const contrib = require('blessed-contrib')
-const screen = blessed.screen()
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
 
-const REDIS_URL = nconf.get('REDIS_URL') || 'redis://91.121.45.250:6379/1';
+const screen = blessed.screen();
+
+const REDIS_URL = 'redis://localhost:6379/0';
 
 const readClient = redis.createClient(REDIS_URL, {
   retry_strategy() {
@@ -12,8 +12,8 @@ const readClient = redis.createClient(REDIS_URL, {
   },
 });
 
-//readClient.on('connect', () => console.log('readClient connected at ' + REDIS_URL));
-//readClient.on('ready', () => console.log('readClient ready'));
+// readClient.on('connect', () => console.log('readClient connected at ' + REDIS_URL));
+// readClient.on('ready', () => console.log('readClient ready'));
 
 const table = contrib.table({
   keys: true,
@@ -23,65 +23,73 @@ const table = contrib.table({
   interactive: true,
   label: 'Monitored keys',
   width: '100%',
-  height: '100%',
+  height: '80%',
   border: { type: 'line', fg: 'cyan' },
-  columnSpacing: 10, //in chars,
-  columnWidth: [20, 12, 12] /*in chars*/
+  columnSpacing: 10, // in chars,
+  columnWidth: [20, 12, 12, 100], /* in chars */
 });
 
 table.focus();
 screen.append(table);
 
 const listenedKeys = {
-  'waiting.general': 'list',
-  'waiting.fcm': 'list',
-  'waiting.apns': 'list',
-  'waiting.sms': 'list',
-  'sent.fcm': 'sset',
-  'sent.apns': 'sset',
-  'sent.sms': 'sset',
-  'delivered.fcm': 'list',
-  'delivered.apns': 'list',
-  'delivered.sms': 'list',
-  'delivered.success': 'list',
-  'delivered.failure': 'list',
-  'delivered.queue': 'sset',
-}
+  key1: 'list',
+  key2: 'zset',
+  key3: 'string',
+  key4: 'hash',
+};
 
-const headers = ['Key name', 'Key type', 'Key length'];
+const headers = ['Key name', 'Key type', 'Command', 'Key value'];
+
+const commands = {
+  zset: ['ZCOUNT', ['-inf', '+inf']],
+  list: ['LLEN'],
+  string: ['GET'],
+  hash: ['HLEN'],
+};
 
 const keysNames = Object.keys(listenedKeys);
 
 let displayArray;
 
 function checkKeys() {
-  const sentArray = keysNames.map(name => {
-    if (listenedKeys[name] === 'list')
-      return ['llen', name]
-    else if (listenedKeys[name] === 'sset')
-      return ['zcount', name, '-inf', '+inf']
-    else
-      throw new Error('wtf? ' + name)
-  })
+  const sentArray = keysNames.map((name) => {
+    const keyType = listenedKeys[name];
+
+    if (!commands[keyType]) {
+      throw new Error(`Not valid key type: ${keyType}, valid are ${Object.keys(commands).join(', ')}`);
+    }
+
+    if (commands[keyType].length > 1) {
+      return [commands[keyType][0], name, ...commands[keyType][1]];
+    }
+
+    return [commands[keyType][0], name];
+  });
 
   readClient.batch(sentArray).exec((err, replies) => {
     displayArray = replies.map((reply, index) => {
-      return [keysNames[index], listenedKeys[keysNames[index]], reply]
-    })
+      const keyName = keysNames[index];
+      const keyType = listenedKeys[keyName];
+      const command = commands[keyType][0];
+      const response = reply || '[nil]';
+
+      return [keyName, keyType, command, response];
+    });
 
     table.setData({
-      headers: headers,
+      headers,
       data: displayArray,
-    })
+    });
 
-    screen.render()
-  })
+    screen.render();
+  });
 }
 
-setInterval(checkKeys, 100)
+setInterval(checkKeys, 100);
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(['escape', 'q', 'C-c'], function escape(ch, key) {
   return process.exit(0);
 });
 
-screen.render()
+screen.render();
