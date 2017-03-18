849527fe4e0c7cf71eefcf906dcf43c37fc74c92
serge1peshcoff <serge1peshcoff@mail.ru>
2017-03-18 13:31:25 +0300

    Moved constants to lib/constants.js and added timestamps

--- bin/redis-lister.js
+++ bin/redis-lister.js
@@ -3,8 +3,11 @@
 const redis = require('redis');
 const path = require('path');
 const fs = require('fs');
+const moment = require('moment');
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
+
+const constants = require('../lib/constants');
 
 if (process.argv.length !== 3) {
   console.log('Usage: redis-lister path/to/config.json');
@@ -21,14 +24,8 @@ try {
   process.exit(1);
 }
 
-const configTypes = {
-  host: 'String',
-  keys: 'Object',
-  updateInterval: 'Number',
-};
-
 // Checking if each field is presented and is the right type.
-Object.keys(configTypes).forEach((field) => {
+Object.keys(constants.configTypes).forEach((field) => {
   if (!config[field]) {
     console.log(`Parse config error: required field '${field} is missing.`);
     process.exit(1);
@@ -37,24 +34,17 @@ Object.keys(configTypes).forEach((field) => {
   const fieldType = Object.prototype.toString.call(config[field]);
   const fieldTypeTrimmed = fieldType.substring(8, fieldType.length - 1);
 
-  if (fieldTypeTrimmed !== configTypes[field]) {
+  if (fieldTypeTrimmed !== constants.configTypes[field]) {
     console.log(`Parse config error: field '${field}' is of the type '${fieldTypeTrimmed}', \
-but '${configTypes[field]}' was expected.`);
+but '${constants.configTypes[field]}' was expected.`);
     process.exit(1);
   }
 });
 
-const commands = {
-  zset: ['ZCOUNT', ['-inf', '+inf']],
-  list: ['LLEN'],
-  string: ['GET'],
-  hash: ['HLEN'],
-};
-
 Object.keys(config.keys).forEach((key) => {
-  if (!commands[config.keys[key]]) {
+  if (!constants.commands[config.keys[key]]) {
     console.log(`Parse config error: wrong key type '${config.keys[key]}' was passed, \
-available types: ${Object.keys(commands).join(', ')}`);
+available types: ${Object.keys(constants.commands).join(', ')}`);
     process.exit(1);
   }
 });
@@ -62,27 +52,16 @@ available types: ${Object.keys(commands).join(', ')}`);
 const screen = blessed.screen();
 const grid = new contrib.grid({ rows: 4, cols: 1, screen });
 
-const table = grid.set(0, 0, 3, 1, contrib.table, {
-  keys: true,
-  fg: 'white',
-  selectedFg: 'white',
-  selectedBg: 'blue',
-  interactive: true,
-  label: 'Monitored keys',
-  width: '100%',
-  height: '80%',
-  border: { type: 'line', fg: 'cyan' },
-  columnSpacing: 10, // in chars,
-  columnWidth: [20, 12, 12, 100], /* in chars */
-});
+const table = grid.set(0, 0, 3, 1, contrib.table, constants.tableOptions);
+const log = grid.set(3, 0, 1, 1, contrib.log, constants.logOptions);
 
 table.focus();
+screen.render();
 
-const log = grid.set(3, 0, 1, 1, contrib.log, {
-  fg: 'blue',
-  selectedFg: 'blue',
-  label: 'Redis log',
-});
+function addToLog(string) {
+  const timestamp = moment().format('MMMM Do YYYY, HH:mm:ss');
+  log.log(`${timestamp}  ${string}`);
+}
 
 const redisClient = redis.createClient(config.host, {
   retry_strategy() {
@@ -90,13 +69,12 @@ const redisClient = redis.createClient(config.host, {
   },
 });
 
-redisClient.on('connect', () => log.log(`Redis client connected at ${config.host}`));
-redisClient.on('ready', () => log.log('Redis client ready'));
-redisClient.on('error', err => log.log(`{red-fg}Redis client error: ${err}{/red-fg}`));
-redisClient.on('reconnecting', () => log.log('Redis client reconnecting...'));
-redisClient.on('end', () => log.log('Redis client ended.'));
+redisClient.on('connect', () => addToLog(`Redis client connected at ${config.host}`));
+redisClient.on('ready', () => addToLog('Redis client ready'));
+redisClient.on('error', err => addToLog(`{red-fg}Redis client error: ${err}{/red-fg}`));
+redisClient.on('reconnecting', () => addToLog('{yellow-fg}Redis client reconnecting...{/yellow-fg}'));
+redisClient.on('end', () => addToLog('{red-fg}Redis client ended.{/red-fg}'));
 
-const headers = ['Key name', 'Key type', 'Command', 'Key value'];
 const keysNames = Object.keys(config.keys);
 let displayArray;
 
@@ -104,29 +82,29 @@ function checkKeys() {
   const sentArray = keysNames.map((name) => {
     const keyType = config.keys[name];
 
-    if (!commands[keyType]) {
-      throw new Error(`Not valid key type: ${keyType}, valid are ${Object.keys(commands).join(', ')}`);
+    if (!constants.commands[keyType]) {
+      throw new Error(`Not valid key type: ${keyType}, valid are ${Object.keys(constants.commands).join(', ')}`);
     }
 
-    if (commands[keyType].length > 1) {
-      return [commands[keyType][0], name, ...commands[keyType][1]];
+    if (constants.commands[keyType].length > 1) {
+      return [constants.commands[keyType][0], name, ...constants.commands[keyType][1]];
     }
 
-    return [commands[keyType][0], name];
+    return [constants.commands[keyType][0], name];
   });
 
   redisClient.batch(sentArray).exec((err, replies) => {
     displayArray = replies.map((reply, index) => {
       const keyName = keysNames[index];
       const keyType = config.keys[keyName];
-      const command = commands[keyType][0];
+      const command = constants.commands[keyType][0];
       const response = (reply != undefined) ? reply : '[nil]';
 
       return [keyName, keyType, command, response];
     });
 
     table.setData({
-      headers,
+      headers: constants.tableHeaders,
       data: displayArray,
     });
 
@@ -136,8 +114,4 @@ function checkKeys() {
 
 setInterval(checkKeys, config.updateInterval);
 
-screen.key(['escape', 'q', 'C-c'], function escape(ch, key) {
-  return process.exit(0);
-});
-
-screen.render();
+screen.key(['escape', 'q', 'C-c'], () => process.exit(0));
